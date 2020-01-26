d627ba8bc3ab13809ee5057c76c68f655ff734bd
Ian Gutwinski <iangutwinski@gmail.com>
2020-01-26 21:32:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
    FROM projects
    LEFT JOIN pledges
    ON projects.id = pledges.project_id
-   WHERE pledges.amount >= projects.funding_goal
+   GROUP BY projects.title
+   HAVING pledges.amount >= projects.funding_goal
   ;"
 end
 
